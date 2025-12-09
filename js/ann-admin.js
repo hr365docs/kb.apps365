b3526532c8144c58d4cb9d796a7be1245af64d0c
Vidya200230 <poojarividya690@gmail.com>
2025-12-09 17:19:23 +0530

    work

--- js/ann-admin.js
+++ js/ann-admin.js
@@ -4,35 +4,35 @@ $(document).ready(function () {
     $("#polladmin").append(
         // ✅ Introduction
         "<li class='sidelist' data-nav-id='./Settings/' title='Introduction'>" +
-            "<a href='/Anniversary-365/modern/admin/introduction/'>Introduction</a>" +
+            "<a href='/Work-Anniversary-365/modern/admin/introduction/'>Introduction</a>" +
         "</li>" +
 
         // ✅ Home Page
         "<li class='sidelist' data-nav-id='./Settings/' title='Home Page'>" +
-            "<a href='/Anniversary-365/modern/admin/homepage/'>Home Page</a>" +
+            "<a href='/Work-Anniversary-365/modern/admin/homepage/'>Home Page</a>" +
         "</li>" +
 
         // ✅ Settings
         "<li class='dropdownicon sidelist' title='Settings'>" +
-    "<a href='/Anniversary-365/modern/admin/settings/'>Settings</a>" +
+    "<a href='/Work-Anniversary-365/modern/admin/settings/'>Settings</a>" +
     "<img class='helpdesk-img aside_dropdown_icon' src='/sharepoint-employee-onboarding/modern/admin/js-modern-admin/down-arrow-svgrepo-com.svg'>" +
 "</li>" +
 "<li class='sidelist dropdown_romove_dot'>" +
     "<ul class='sidenavSub-topics aside_dropdown_list'>" +
         "<li class='sidelist' title='Sync Date of Joining'>" +
-            "<a href='/Anniversary-365/modern/admin/settings/#_sync_date'>Sync Date of Joining</a>" +
+            "<a href='/Work-Anniversary-365/modern/admin/settings/#_sync_date'>Sync Date of Joining</a>" +
         "</li>" +
         "<li class='sidelist' title='Display Work Anniversaries Based On'>" +
-            "<a href='/Anniversary-365/modern/admin/settings/#_display_anniversaries'>Display Work Anniversaries Based On</a>" +
+            "<a href='/Work-Anniversary-365/modern/admin/settings/#_display_anniversaries'>Display Work Anniversaries Based On</a>" +
         "</li>" +
         "<li class='sidelist' title='Theme'>" +
-            "<a href='/Anniversary-365/modern/admin/settings/#_theme_option'>Theme</a>" +
+            "<a href='/Work-Anniversary-365/modern/admin/settings/#_theme_option'>Theme</a>" +
         "</li>" +
         "<li class='sidelist' title='Hide SharePoint Settings'>" +
-            "<a href='/Anniversary-365/modern/admin/settings/#_hide_sharepoint_settings'>Hide SharePoint Settings</a>" +
+            "<a href='/Work-Anniversary-365/modern/admin/settings/#_hide_sharepoint_settings'>Hide SharePoint Settings</a>" +
         "</li>" +
         "<li class='sidelist' title='Add Additional Admins'>" +
-            "<a href='/Anniversary-365/modern/admin/settings/#_additional_admins'>Add Additional Admins</a>" +
+            "<a href='/Work-Anniversary-365/modern/admin/settings/#_additional_admins'>Add Additional Admins</a>" +
         "</li>" +
     "</ul>" +
 "</li>" +
@@ -40,13 +40,13 @@ $(document).ready(function () {
 
         // ✅ Help
         "<li class='dropdownicon sidelist' title='Help'>" +
-            "<a href='/Anniversary-365/modern/admin/help/'>Help</a>" +
+            "<a href='/Work-Anniversary-365/modern/admin/help/'>Help</a>" +
               "<img class='helpdesk-img aside_dropdown_icon' src='/sharepoint-employee-onboarding/modern/admin/js-modern-admin/down-arrow-svgrepo-com.svg'>" +
         "</li>" +
         "<li class='sidelist dropdown_romove_dot'>" +
             "<ul class='sidenavSub-topics aside_dropdown_list'>" +
                 "<li class='sidelist' title='Product Activation'>" +
-                    "<a href='/Anniversary-365/modern/admin/help/#product'>Product Activation</a>" +
+                    "<a href='/Work-Anniversary-365/modern/admin/help/#product'>Product Activation</a>" +
                 "</li>" +
             "</ul>" +
         "</li>"
