d214a07485a348cdc274eb730984d27a234d4cd1
Vidya200230 <poojarividya690@gmail.com>
2026-01-27 16:10:12 +0530

    survey

--- js/task-modern-admin.js
+++ js/task-modern-admin.js
@@ -187,8 +187,8 @@ $(document).ready(function () {
         "<li data-nav-id='../advanced/' title='Timesheet 365' class='sidelist'>" +
         "<a href='/task-management-365/modern/admin/settings/integration/#Timesheet-365'>Timesheet 365</a>" +
         "</li>" +
-        "<li data-nav-id='../advanced/' title='Expense Tracker 365' class='sidelist'>" +
-        "<a href='/task-management-365/modern/admin/settings/integration/#Expense-Tracker-365'>Expense Tracker 365</a>" +
+        "<li data-nav-id='../advanced/' title='Expense 365' class='sidelist'>" +
+        "<a href='/task-management-365/modern/admin/settings/integration/#Expense-Tracker-365'>Expense 365</a>" +
         "</li>" +
         "<li data-nav-id='../advanced/' title='Recruitment Management 365' class='sidelist'>" +
         "<a href='/task-management-365/modern/admin/settings/integration/#RM365-365'>Recruitment Management 365</a>" +
