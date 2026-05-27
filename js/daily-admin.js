461f944454a82469a04af3f1dee740e2795cc8d7
vidya.p <vidya.p@apps365.com>
2026-05-27 17:37:33 +0530

    new webpart

--- js/daily-admin.js
+++ js/daily-admin.js
@@ -4,42 +4,42 @@ $(document).ready(function () {
     $("#polladmin").append(
         // ✅ Introduction
         "<li class='sidelist' data-nav-id='./Settings/' title='Introduction'>" +
-            "<a href='/Daily-Tips-36/modern/admin/introduction/'>Introduction</a>" +
+            "<a href='/daily-tips-36/modern/admin/introduction/'>Introduction</a>" +
         "</li>" +
 
         // ✅ Home Page
         "<li class='sidelist' data-nav-id='./Settings/' title='Home Page'>" +
-            "<a href='/Daily-Tips-36/modern/admin/homepage/'>Home Page</a>" +
+            "<a href='/daily-tips-36/modern/admin/homepage/'>Home Page</a>" +
         "</li>" +
 
         // ✅ Settings
         "<li class='dropdownicon sidelist' title='Settings'>" +
-            "<a href='/Daily-Tips-36/modern/admin/settings/'>Settings</a>" +
+            "<a href='/daily-tips-36/modern/admin/settings/'>Settings</a>" +
               "<img class='helpdesk-img aside_dropdown_icon' src='/sharepoint-employee-onboarding/modern/admin/js-modern-admin/down-arrow-svgrepo-com.svg'>" +
         "</li>" +
         "<li class='sidelist dropdown_romove_dot'>" +
             "<ul class='sidenavSub-topics aside_dropdown_list'>" +
                 "<li class='sidelist' title='Theme'>" +
-                    "<a href='/Daily-Tips-36/modern/admin/settings/#_theme_option'>Theme</a>" +
+                    "<a href='/daily-tips-36/modern/admin/settings/#_theme_option'>Theme</a>" +
                 "</li>" +
                 "<li class='sidelist' title='Users'>" +
-                    "<a href='/Daily-Tips-36/modern/admin/settings/#_users'>Users</a>" +
+                    "<a href='/daily-tips-36/modern/admin/settings/#_users'>Users</a>" +
                 "</li>" +
                 "<li class='sidelist' title='Hide SharePoint Settings'>" +
-                    "<a href='/Daily-Tips-36/modern/admin/settings/#_hide_sharepoint_settings'>Hide SharePoint Settings</a>" +
+                    "<a href='/daily-tips-36/modern/admin/settings/#_hide_sharepoint_settings'>Hide SharePoint Settings</a>" +
                 "</li>" +
             "</ul>" +
         "</li>" +
 
         // ✅ Help
         "<li class='dropdownicon sidelist' title='Help'>" +
-            "<a href='/Daily-Tips-36/modern/admin/help/'>Help</a>" +
+            "<a href='/daily-tips-36/modern/admin/help/'>Help</a>" +
               "<img class='helpdesk-img aside_dropdown_icon' src='/sharepoint-employee-onboarding/modern/admin/js-modern-admin/down-arrow-svgrepo-com.svg'>" +
         "</li>" +
         "<li class='sidelist dropdown_romove_dot'>" +
             "<ul class='sidenavSub-topics aside_dropdown_list'>" +
                 "<li class='sidelist' title='Product Activation'>" +
-                    "<a href='/Daily-Tips-36/modern/admin/help/#product'>Product Activation</a>" +
+                    "<a href='/daily-tips-36/modern/admin/help/#product'>Product Activation</a>" +
                 "</li>" +
             "</ul>" +
         "</li>"
